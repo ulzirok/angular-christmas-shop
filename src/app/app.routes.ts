b425c7d5bb5c753faa743ea41788858646e29418
ulzirok <ulzirok@gmail.com>
2025-09-06 23:13:15 +0500

    add files for variables, mixins, fonts and set global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Gifts } from './features/gifts/gifts';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  {path: 'gifts', component: Gifts}
+];

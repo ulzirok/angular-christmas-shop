8cc30bcb25cdf474f41ec20a048a4bc48ae85c70
ulzirok <ulzirok@gmail.com>
2025-09-10 17:03:15 +0500

    add giftsService for dynamic display of a gift-card on the gift page and random display on the home page

--- src/app/features/gifts/gifts-list/gifts-list.ts
+++ src/app/features/gifts/gifts-list/gifts-list.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GiftCard } from '../../../shared/gift-card/gift-card';
+import { GiftDetails } from '../gift-details/gift-details';
+import { IGift } from '../../../models/gift-model';
+import { CommonModule } from '@angular/common';
+import { GiftsService } from '../../../services/gifts-service';
 
 @Component({
   selector: 'app-gifts-list',
-  imports: [GiftCard],
+  imports: [GiftCard, GiftDetails, CommonModule],
   templateUrl: './gifts-list.html',
   styleUrl: './gifts-list.scss'
 })
-export class GiftsList {
-
+export class GiftsList implements OnInit {
+  gifts: IGift[] = []
+  
+  constructor(private giftsService: GiftsService) {  }
+  
+  ngOnInit(): void {
+    this.giftsService.getAll().subscribe((gifts) => {
+      this.gifts = gifts
+      
+    })
+  }
 }
--- src/app/features/home/gifts-preview/gifts-preview.ts
+++ src/app/features/home/gifts-preview/gifts-preview.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GiftCard } from '../../../shared/gift-card/gift-card';
+import { IGift } from '../../../models/gift-model';
+import { GiftsService } from '../../../services/gifts-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gifts-preview',
-  imports: [GiftCard],
+  imports: [GiftCard, CommonModule],
   templateUrl: './gifts-preview.html',
   styleUrl: './gifts-preview.scss'
 })
-export class GiftsPreview {
-
+export class GiftsPreview implements OnInit {
+  gifts: IGift[] = []
+  
+  constructor(private giftsService: GiftsService) { }
+  
+  ngOnInit(): void {
+    this.giftsService.getRandom(4).subscribe((gifts) => {
+      this.gifts = gifts
+    })
+  }
 }

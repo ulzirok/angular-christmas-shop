4b485b8838d37d03af7e3c8b9cd1ef7ccc7e8fbf
ulzirok <ulzirok@gmail.com>
2025-09-17 15:45:39 +0500

    add a reactive form for a simple form on the Contacts-page

--- src/app/features/contacts/contact-form/contact-form.ts
+++ src/app/features/contacts/contact-form/contact-form.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact-form.html',
-  styleUrl: './contact-form.scss'
+  styleUrls: ['./contact-form.scss']
 })
 export class ContactForm {
-
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required, Validators.minLength(10)])
+  })
+  
+  @Output() submittedSuccess = new EventEmitter<void>()
+  
+  onSubmit() {
+    if (this.form.invalid) {
+      return
+    }
+    else {
+      this.submittedSuccess.emit()
+      this.form.reset()
+    }
+  }
 }
